4fe517dd803e418791ae5bd74ee77c90db05ceab
Keneuoe Nkuatsana <nkuatsanakeneuoe@gmail.com>
2023-02-16 12:49:30 -0800

    Hello puts task done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - A program that uses puts function and ends with a new line
  * Return: Always 0 (Success)
  */
-
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
