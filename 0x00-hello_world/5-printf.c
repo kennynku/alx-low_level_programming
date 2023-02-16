24a145b43f85fd628f4d51a035dedc028569c0e0
Keneuoe Nkuatsana <nkuatsanakeneuoe@gmail.com>
2023-02-16 13:34:50 -0800

    Hello printf task done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Using the print command and ending with a new line
+ * Return: Always (0) (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
