ca553700b7b9c2fb40c7d4fb35b995445afd455c
Keneuoe Nkuatsana <nkuatsanakeneuoe@gmail.com>
2023-02-17 09:50:53 -0800

    AlphABET task done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Printing alphabets in both small and large cases
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++);
+	putchar(alp);
+	for (alp = 'A'; alp <= 'Z'; alp++);
+	putchar(alp);
+	putchar('\n');
+	return (0);
+
+}
