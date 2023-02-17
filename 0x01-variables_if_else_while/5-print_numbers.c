b7c08ad3702334a4fa99798eaa05bdb339af10b2
Keneuoe Nkuatsana <nkuatsanakeneuoe@gmail.com>
2023-02-17 11:31:28 -0800

    Numbers task done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - writing numbers of base 10 starting from 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
