b8b80824d95b2ff5f1ce861b80efb761f478cc49
Keneuoe Nkuatsana <nkuatsanakeneuoe@gmail.com>
2023-02-23 04:57:02 -0800

    numbers task done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Printing numbers from 0 to 9
+ * Return: Always 0 (Success)
+ */
+void print_numbers(void)
+{
+	int k;
+
+	for (k = 48; k < 58; k++)
+	{
+		_putchar(k);
+	}
+	_putchar('\n');
+}
+
