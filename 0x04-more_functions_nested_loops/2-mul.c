0d9c0874506f787bf91d8d634ecdbe5e4e00a2ec
Keneuoe Nkuatsana <nkuatsanakeneuoe@gmail.com>
2023-02-23 04:32:42 -0800

    Multiplication task done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplying 2 intergers
+ * @a: first interger
+ * @b: second interger
+ * Return: addition of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
