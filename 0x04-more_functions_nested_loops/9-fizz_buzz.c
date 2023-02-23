b9966014631e27ca76f2c634f5c8f80c2f8fcbcb
Keneuoe Nkuatsana <nkuatsanakeneuoe@gmail.com>
2023-02-23 06:07:16 -0800

    FizzBuzz task done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - printing the numbers from 1 to 100
+ * for multiples of 3 print Fizz
+ * for multiples of 5 print Buzz
+ * for both the multiplease of 3 and 5 print FizzBuzz
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
