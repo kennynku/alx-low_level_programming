d4f84e32b2e0c0c996387d89209791b51357532d
Keneuoe Nkuatsana <nkuatsanakeneuoe@gmail.com>
2023-02-27 07:58:37 -0800

    98 Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 - writing a function that takes a pointer to an int and updates the value it points to to 98
+ * reset_to_98 - writing a function that takes a pointer to an int
  * @n: input
  * Return: n
  */
