bd7237a9676e7a77f282e450cd83f3e08897816d
Keneuoe Nkuatsana <nkuatsanakeneuoe@gmail.com>
2023-02-27 08:57:17 -0800

    Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - printing in reverse
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
