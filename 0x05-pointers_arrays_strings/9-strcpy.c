d4fc8608a0b5782f756945a21b610cf86ebb8e02
Keneuoe Nkuatsana <nkuatsanakeneuoe@gmail.com>
2023-02-27 15:50:20 -0800

    done

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,17 +8,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-	int x = 0;
+	int i;
 
-	while (*(src + i) != '\0')
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		i++;
+		*(dest + i) =  *(src + i);
 	}
-	for (; x < i; x++)
-	{
-		dest[x] = src[x]
-	}
-	dest[i] = '\0';
+	*(dest + i) = '\0';
 	return (dest);
 }
