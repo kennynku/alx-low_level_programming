29a914d49d79aad4ab0bf7e04fa9e7479620353a
Keneuoe Nkuatsana <nkuatsanakeneuoe@gmail.com>
2023-02-27 08:19:24 -0800

    done

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,12 +4,11 @@
  * swap_int - swapping the values of two integers
  * @a: first integer
  * @b: second integer
+ * Return: void
  */
 void swap_int(int *a, int *b)
 {
-	int k;
-
-	k = *a;
-	*a = *b;
-	*b = m;
+	*a += *b;
+	*b = *a - *b;
+	*a = *a - *b;
 }
