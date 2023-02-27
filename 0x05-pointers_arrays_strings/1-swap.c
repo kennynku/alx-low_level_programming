f024e75d8cfd18c66530d18fc774d6bcf91d28b0
Keneuoe Nkuatsana <nkuatsanakeneuoe@gmail.com>
2023-02-27 08:03:38 -0800

    Swap horses task done

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swapping the values of two integers
+ * @a: first integer
+ * @b: second integer
+ */
+void swap_int(int *a, int *b)
+{
+	int k;
+
+	k = *a;
+	*a = *b;
+	*b = m;
+}
