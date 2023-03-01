90b09b689afe59bc544c06e379fc8b79eb751b99
Keneuoe Nkuatsana <nkuatsanakeneuoe@gmail.com>
2023-03-01 11:16:16 -0800

    done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,6 @@ char *cap_string(char *str)
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
-
 		if (str[i - 1] == '' ||
 		str[i - 1] == '\t' ||
 		str[i - 1] == '\n' ||
@@ -23,14 +22,13 @@ char *cap_string(char *str)
 		str[i - 1] == '!' ||
 		str[i - 1] == '?' ||
 		str[i - 1] == '"' ||
-		str[i - 1] == '('||
+		str[i - 1] == '(' ||
 		str[i - 1] == ')' ||
 		str[i - 1] == '{' ||
 		str[i - 1] == '}' ||
-		i == 0)
-			str[i] -=32;
+		i == 0);
+		str[i] -= 32;
 		i++;
 	}
-
 	return (str);
 }
