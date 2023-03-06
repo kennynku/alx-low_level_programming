67726ebcb25c7fa1122c48e6b97770e5ad0cc545
Keneuoe Nkuatsana <nkuatsanakeneuoe@gmail.com>
2023-03-06 06:31:16 -0800

    chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,12 +3,13 @@
 /**
  * print_chessboard - printing a chessboard
  * @a: chessboard to be printed
+ * Return: Always 0 (success)
  */
 void print_chessboard(char (*a)[8])
 {
 	int w, z;
 
-	for (w = 0; w < 8; w++)
+	for (w = 0; a[w][7]; w++)
 	{
 		for (z = 0, z < 8; z++)
 			_putchar(a[w][z]);
