59841667fdb6abe147e53d9b402d3adcf558b9c5
Keneuoe Nkuatsana <nkuatsanakeneuoe@gmail.com>
2023-03-13 13:23:11 -0700

    argstostr

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,35 +10,40 @@
 char *argstostr(int ac, char **av)
 {
 	char *str;
-	int i, j, k, len;
+	int i;
+	int j;
+	int len = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		j = 0;
+
+		while (av[i][j++])
 			len++;
 	}
+	len++;
 
-	str = malloc(sizeof(char) * (len + 1));
+	str = malloc(sizeof(**av) * (len + ac));
 
 	if (str == NULL)
 		return (NULL);
 
-	k = 0;
+	len = 0;
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			str[k] = av[i][j];
-			k++;
-		}
+		j = 0;
 
-		str[k] = '\n';
-		k++;
+		while (av[i][j])
+			str[len++] = av[i][j++];
+
+		str[len++] = '\n';
 	}
+
+	str[len] = '\0';
 
 	return (str);
 }
