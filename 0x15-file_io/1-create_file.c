9482da6929e01e69641192483ddab1b35e113207
Keneuoe Nkuatsana <nkuatsanakeneuoe@gmail.com>
2023-04-08 12:53:01 -0700

    ceate file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	b = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
+	b = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	u = write(b, text_content, len);
 
