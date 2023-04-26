3c932c03bb7fa3afca0cffb7cb97e4b9dbd51461
kennynku <nkuatsanakeneuoe@gmail.com>
2023-04-26 02:02:28 -0700

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,18 +11,17 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int k = 0;
-	int dk;
+	unsigned int i = 0, val = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
-
-	for (dk = 0; b[dk]; dk++)
+	for (; *(b + i); i++)
 	{
-		if (b[dk] < '0' || b[dk] > '1')
+		val = val << 1;
+		if (*(b + i) == '1')
+			val = val | 1;
+		else if (*(b + i) != '0')
 			return (0);
-		k = 2 * k + (b[dk] - '0');
 	}
-	return (k);
+	return (val);
 }
-
